9045fcf015bac1b7debd4ca7768c193c249924f8
Daniele Oliveira <droliver.dev@gmail.com>
2021-02-26 13:06:39 -0300

    refatoração das rotas

--- api/routes.js
+++ api/routes.js
@@ -1,5 +1,11 @@
-module.exports = {
-  getPosts: require('../controllers/posts'),
-  createUsers: require('../controllers/createUser'),
-  login: require('../controllers/login'),
-};
+const express = require('express');
+const createProduct =  require('../controllers/createProduct');
+const createUsers = require('../controllers/createUser');
+const login = require('../controllers/login');
+
+const apiRoutes = express.Router();
+apiRoutes.post('/api/users', createUsers);
+apiRoutes.post('/api/login', login);
+apiRoutes.post('/api/products', createProduct);
+
+module.exports = apiRoutes;
